1e80cd3931f92242d794281e1b1d512c74b6545a
Sudipta Gupta <sudiptagupta217@gmail.com>
2021-05-13 15:42:07 +0530

    initial commit create video player

---
+++ app/src/main/java/com/sudipta/videoplay/MainActivity.java
@@ -0,0 +1,25 @@
+package com.sudipta.videoplay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        VideoView videoView = findViewById(R.id.videoView);
+        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.cat);
+        MediaController mediaController = new MediaController(this);
+        videoView.setMediaController(mediaController);
+        mediaController.setAnchorView(videoView);
+
+
+        videoView.start();
+    }
+}
